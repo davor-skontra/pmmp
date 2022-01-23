0e702971ebde6ee2b012db4918ca009396510fe2
theKrad23 <thekrad23@gmail.com>
2022-01-23 17:01:46 +0200

    Add export button and transitions to palette abd export button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React, {CSSProperties, useEffect, useState} from 'react';
+import React, {CSSProperties, useEffect, useRef, useState} from 'react';
 import './App.css';
-import {Box, Card, Collapse, Paper, Switch, Typography} from "@mui/material";
+import {Box, Card, Collapse, Fab, Paper, Slide, Switch, Typography} from "@mui/material";
 import ColorPropertySelector from "./ColorPropertySelector";
 import {calculateColors, ColorCalculationSettings, ColorColumn, ColorProperty} from "./palette-engine";
 import _ from 'lodash'
 import ColorPropertyConstantSelector from "./ColorPropertyConstantSelector";
 import ColorPropertyRange from "./color-property-range";
 import LabeledCheckbox from "./LabeledCheckbox";
+import {SaveAs} from "@mui/icons-material";
 
 const cardStyle: CSSProperties = {
     margin: '10pt',
@@ -32,7 +33,7 @@ function App() {
     const [constantValue, setConstantValue] = useState(50) // Middle value (between 0 and 100) for constant
     
     const [showDebugValues, setShowDebugValues] = useState(false)
-    
+    const colors = useRef<ColorColumn[]>([])
     
     const settings: ColorCalculationSettings = {
         baseColorProperty: baseSelection,
@@ -50,21 +51,33 @@ function App() {
     const amountOfColorProperties = 3
     const allSelections = [baseSelection, variantSelection, constantSelection]
     let selectionsAreValid = _.uniq(allSelections).length === amountOfColorProperties
-    let colorRows: ColorColumn[] = [];
     
     if (selectionsAreValid) {
-        colorRows = calculateColors(settings)
-        selectionsAreValid = !_.isEmpty(colorRows)
+        colors.current = calculateColors(settings)
+        selectionsAreValid = !_.isEmpty(colors)
     }
     
     useEffect(() => {
-        document.title = "pmmp - Make Palettes"
+        document.title = 'pmmp - Make Palettes'
     });
     
     const colorFieldStyle: CSSProperties = {
         height: `30pt`,
         width: `100%`
     }
+    
+    const fabStyle: CSSProperties = {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: '10pt',
+        width: '150pt',
+        marginLeft: '10pt'
+    }
+    
+    const validSelectionTransition = (delayWhen: boolean) : CSSProperties => ({
+        transitionDelay: (delayWhen ? '300ms' : '0ms'),
+        transitionDuration: '100ms'
+    })
     
     return (
         <Box>
@@ -74,7 +87,7 @@ function App() {
                     <LabeledCheckbox defaultChecked={showDebugValues} label={'Show Debug Values'} onChange={(e, v) => setShowDebugValues(v)}/>
                 </Box>
             </Card>
-            <Card style={{...cardStyle, flexDirection: 'column'}}>
+            <Card style={{...cardStyle, flexDirection: 'column', justifyContent: 'space-evenly'}}>
                 <ColorPropertySelector
                     title={'Base:'}
                     defaultSelected={baseSelection}
@@ -111,18 +124,23 @@ function App() {
                     </Card>
                 </Collapse>
             </Card>
-            <Collapse in={selectionsAreValid}>
+            <Slide direction='right' in={selectionsAreValid} mountOnEnter unmountOnExit style={validSelectionTransition(!selectionsAreValid)}>
                 <Card style={cardStyle}>
                     <Box style={{display: 'flex', flexDirection: 'row', width: '100%'}}>
-                        {colorRows.map(r =>
+                        {colors.current.map(r =>
                             <div style={{display: 'flex', flexDirection: 'column', width: "100%", flexWrap: 'wrap'}}>
                                 {r.colors.flatMap(c => (<div style={{...colorFieldStyle, backgroundColor: c.hex()}}/>))}
                             </div>
                         )}
                     </Box>
-                        
                 </Card>
-            </Collapse>
+            </Slide>
+            <Slide direction='right' in={selectionsAreValid} mountOnEnter unmountOnExit style={validSelectionTransition(selectionsAreValid)}>
+                <Fab variant='extended' color='secondary' style={fabStyle}>
+                    <SaveAs/>
+                    Export Palette
+                </Fab>
+            </Slide>
         </Box>
     );
 }
